6fc58bcb79012cef4901ba7f32091a1cb9f9ffed
syaifulghifari <syaiful.ghifari12@gmail.com>
2022-07-24 11:00:59 +0700

    Login

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -4,13 +4,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Detail from '../pages/Detail';
+import Registration from '../pages/Registration';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='home' element={<Home />} />
+        <Route path='/' element={<Home />} />
+        <Route path='Login' element={<Login />} />
+        <Route path='registration' element={<Registration />} />
         <Route path='detail' element={<Detail />} />
         <Route path='*' element={<div>404 Error Not Found</div>} />
       </Routes>
